ee8a25dd979880349974221d53088e5ee5e7268c
Rupert Walker <RupertCW@gmail.com>
2024-02-24 21:53:33 +0000

    Update workflow-build.ps1

--- build-tools/workflow-build.ps1
+++ build-tools/workflow-build.ps1
@@ -1,30 +1,7 @@
-Function Get-TopChildItem($Path, $Filter) {
-    $Results = [System.Collections.Generic.List[String]]::New()
-    $ProcessingQueue = [System.Collections.Queue]::new()
-
-    ForEach ($item in (Get-ChildItem -Directory $Path)) {
-        $ProcessingQueue.Enqueue($item.FullName) 
-    }    
-
-    While ($ProcessingQueue.Count -gt 0) {
-            $Item = $ProcessingQueue.Dequeue()
-
-            if ($Item -match $Filter) {
-                    $Results.Add($Item) 
-            }
-            else {
-                    ForEach ($el in (Get-ChildItem -Path $Item -Directory)) {
-                            $ProcessingQueue.Enqueue($el.FullName) 
-                    }
-            }
-    }
-    return $Results
-}
-
 Invoke-WebRequest "http://adoxa.altervista.org/freelancer/dlt.php?f=xmlproject" -OutFile ${github.workspace}\xmlproject.zip
 Expand-Archive ${github.workspace}\xmlproject.zip
 $destination = "staging\mod-assets\DATA"
-$files = Get-TopChildItem -Path "staging\mod-assets\XML" -Filter '.xml'
+$files = Get-ChildItem -Path "staging\mod-assets\XML" -Filter '*.xml'
 
 $func = {
 param(     
