66f621920ebeb55fe721467dd9fac22d8070bf96
Rupert Walker <RupertCW@gmail.com>
2024-02-24 22:43:38 +0000

    Update workflow-build.ps1

--- build-tools/workflow-build.ps1
+++ build-tools/workflow-build.ps1
@@ -14,7 +14,7 @@ Start-Process -FilePath "$proc" -Wait -ArgumentList $params
 }
 
 foreach($file in $files){
-  Start-Job -ScriptBlock $func -Arg @("${github.workspace}\xmlproject\XMLUTF.exe", "-o $destination $($file.FullName)")
+Start-Process -FilePath "${github.workspace}\xmlproject\XMLUTF.exe", -ArgumentList "-o", "$destination", "$($file.FullName)"
   Write-Host $destination
   Write-Host $file.FullName
   Write-Host $func
