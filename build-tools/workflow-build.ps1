960b6410e3a15274b084d2c8e685d185d8c18b34
Rupert Walker <RupertCW@gmail.com>
2024-02-26 15:26:03 +0000

    Delete workflow-build.ps1

--- build-tools/workflow-build.ps1
+++
@@ -1,7 +0,0 @@
-Invoke-WebRequest "http://adoxa.altervista.org/freelancer/xml2utf.exe" -OutFile ${github.workspace}\xml2utf.exe
-$files = Get-ChildItem -Path "staging\mod-assets\XML" -Filter '*.xml'
-Set-Location "${github.workspace}/staging"
-foreach($file in $files){
-Start-Process -Wait ${github.workspace}\xml2utf.exe -ArgumentList "-o ${github.workspace}/staging/mod-assets/DATA $($file.FullName)"
-  Write-Host "Compiling $($file.FullName) to UTF"
-}
