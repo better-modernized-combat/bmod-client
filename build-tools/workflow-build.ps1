655f92dff18f251403b6dd92794262f29e32cd7c
Rupert Walker <RupertCW@gmail.com>
2024-02-26 14:45:03 +0000

    Update workflow-build.ps1

--- build-tools/workflow-build.ps1
+++ build-tools/workflow-build.ps1
@@ -1,8 +1,7 @@
 Invoke-WebRequest "http://adoxa.altervista.org/freelancer/xml2utf.exe" -OutFile ${github.workspace}\xml2utf.exe
-$destination = "${github.workspace}\staging\mod-assets\DATA"
 $files = Get-ChildItem -Path "staging\mod-assets\XML" -Filter '*.xml'
 
 foreach($file in $files){
-Start-Process -Wait ${github.workspace}\xml2utf.exe -ArgumentList "-o $destination $($file.FullName)"
+Start-Process -Wait ${github.workspace}\xml2utf.exe -ArgumentList "-o ${github.workspace}/staging/mod-assets/DATA $($file.FullName)"
   Write-Host "Compiling $($file.FullName) to UTF"
 }
