7d1288eede35d48ed291828160d3c1f8ab572929
Rupert Walker <RupertCW@gmail.com>
2024-02-26 13:45:13 +0000

    Update workflow-build.ps1

--- build-tools/workflow-build.ps1
+++ build-tools/workflow-build.ps1
@@ -3,6 +3,6 @@ $destination = "${github.workspace}\staging\mod-assets\DATA"
 $files = Get-ChildItem -Path "staging\mod-assets\XML" -Filter '*.xml'
 
 foreach($file in $files){
-Start-Process -Wait ${github.workspace}\xml2utf.exe -ArgumentList "-o, $destination, $($file.FullName)"
+Start-Process -Wait ${github.workspace}\xml2utf.exe -ArgumentList "-o $destination $($file.FullName)"
   Write-Host "Compiling $($file.FullName) to UTF"
 }
