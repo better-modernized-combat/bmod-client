55ed30ca00f9f2615a979e9ace4adc974a46fc37
FJonske <frederic.jonske@rwth-aachen.de>
2024-04-18 22:24:09 +0200

    lmao

--- build-tools/python/ini_utils.py
+++ build-tools/python/ini_utils.py
@@ -84,7 +84,6 @@ def write_block(block: pd.Series, block_name: str, cols: pd.Index, out: ContextM
                     continue
                 out.write(col+" = "+pretty_numbers(subvalue)+"\n")
         else:
-            print(col, value.strip() in ["", "nan"] or col == "Comment")
             if value.strip() in ["", "nan"] or col == "Comment":
                 continue
             out.write(col+" = "+pretty_numbers(value)+"\n")
